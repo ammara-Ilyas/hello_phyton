a7d3c29f70c68e408d6b9434896f58aad4b77322
ammara-Ilyas <ismarailyas545@gmail.com>
2024-07-02 21:58:25 +0500

    Loop in phyton

---
+++ loop.py
@@ -0,0 +1,42 @@
+# #  while loop  
+# i=0
+# while i < 5:
+#     print("i",i)
+#     i = i+1
+# i = 1
+
+# y=0
+# while y<10:
+#     print(1*y)
+#     y+=1
+# else:
+#     print("i is longer")    
+    
+# #for loop
+# fruits = ["apple", "banana", "cherry","grapes"]
+# for x in fruits:
+#     print(x)
+#     if x=="cherry":
+#       break
+  
+# for x in range(5):
+#     print(x,"x")  
+    
+# for x in range(2, 6):
+#   print(x)    
+  
+  
+  #tabel of 3
+for x in range(3,31,3):
+    print(x)  
+i=1
+while i<10:    
+   for x in range(4,44,4):
+     print("4 *",i,"=",x)    
+     i+=1    
+     
+     
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")     
