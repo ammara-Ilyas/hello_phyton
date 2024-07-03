e6163d01771ed24c65c69c199fcd216a18a17d1a
ammara-Ilyas <ismarailyas545@gmail.com>
2024-07-03 07:33:49 +0500

    File input output

---
+++ file.py
@@ -0,0 +1,9 @@
+# #Text files   Binary files
+f=open("C:\Users\AMC\Desktop\Phyton\files\demo.txt","r") # "r","w","x","a","t","+ "
+data=f.read()
+print(data)
+# lin1=f.readline()
+# print(lin1)
+
+# #write
+# f=open("C:\Users\AMC\Desktop\Phyton\files\demo.txt","w")
