eae6f9d551ed130175a1118c61ba6ad443758031
ammara-Ilyas <ismarailyas545@gmail.com>
2024-07-28 14:05:26 +0500

    Python list_comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -6,3 +6,12 @@ print(even_numbers)
 numbers = [1, 2, 3, 4, 5, 6]
 squared_even_numbers = map(lambda x: x ** 2, filter(lambda x: x % 2 == 0, numbers))
 print(list(squared_even_numbers))
+
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = [x ** 2 for x in numbers]
+print(squared_numbers)
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = [x for x in numbers if x % 2 == 0]
+print(even_numbers)
